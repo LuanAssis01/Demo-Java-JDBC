dd192caef9511d49ab229f2eb8afe19b959210cd
LuanAssis01 <assisluan863@gmail.com>
2024-05-20 17:40:05 -0300

    Seller entity class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,17 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
     public static void main(String[] args) {
+
         Department obj = new Department(1, "Eletronics");
+        Seller seller = new Seller(20, "Luan", "luanassis@gmail.com", new Date(), 7000.00, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
