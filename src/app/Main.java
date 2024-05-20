128566636116788fd3801e1d0e895d94b73136bb
LuanAssis01 <assisluan863@gmail.com>
2024-05-20 17:56:35 -0300

    SellerDaoJDBC and DaoFactory

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Main {
         Department obj = new Department(1, "Eletronics");
         Seller seller = new Seller(20, "Luan", "luanassis@gmail.com", new Date(), 7000.00, obj);
 
+        SellerDAO sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println(sellerDao);
         System.out.println(obj);
         System.out.println(seller);
     }
